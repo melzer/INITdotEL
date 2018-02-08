ca10d9dde6ec21dfd26e4b099922a66eb84cc96b
Toby Law <michlaw23@gmail.com>
2018-02-08 23:19:22 +1100

    Better commenting

---
+++ elpa/glsl-mode-20170927.736/glsl-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "glsl-mode" "20170927.736" "major mode for Open GLSL shader files" (quote nil))
--- init.el
+++ init.el
@@ -132,6 +132,25 @@
 ;; enable move-text
 (move-text-default-bindings)
 
+;; better commenting
+(defun xah-comment-dwim ()
+  "Like `comment-dwim', but toggle comment if cursor is not at end of line."
+  (interactive)
+  (if (region-active-p)
+      (comment-dwim nil)
+    (let (($lbp (line-beginning-position))
+          ($lep (line-end-position)))
+      (if (eq $lbp $lep)
+          (progn
+            (comment-dwim nil))
+        (if (eq (point) $lep)
+            (progn
+              (comment-dwim nil))
+          (progn
+            (comment-or-uncomment-region $lbp $lep)
+            (forward-line )))))))
+(global-set-key (kbd "C-;") 'xah-comment-dwim)
+
 (custom-set-variables
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen 1)
