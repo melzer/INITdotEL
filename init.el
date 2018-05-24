c139abed51407c795067be04f219eedff8870745
Toby Law <michlaw23@gmail.com>
2018-05-24 20:32:34 +1000

    Cleaned up everything

--- init.el
+++ init.el
@@ -1,14 +1,261 @@
 ;; TODO
-;; syntax checking
-;; magit
+;; learn smartparens
 
-(setq load-prefer-newer t)
-(package-initialize)
-
+;; Packages
+(require 'package)
+(setq package-enable-at-startup nil)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
 
+(defalias 'list-buffers 'ibuffer-other-window)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package org-bullets
+  :ensure t
+  :config (add-hook 'org-mode-hook (lambda () (org-mode-bullets 1))))
+
+(use-package switch-window
+  :ensure t)
+
+(use-package counsel
+  :ensure t
+  :bind
+  (("M-y" . counsel-yank-pop)
+   :map ivy-minibuffer-map
+   ("M-y" . ivy-next-line)))
+
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-display-style 'fancy)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-wrap t))
+
+(use-package monokai-theme
+  :ensure t
+  :config (load-theme 'monokai t))
+
+(use-package undo-tree
+ :ensure t
+ :init (global-undo-tree-mode t))
+
+(use-package move-text
+  :ensure t
+  :init (move-text-default-bindings))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode t))
+
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-center-theme)
+  (setq powerline-default-separator 'wave))
+
+(use-package expand-region
+  :ensure t)
+
+(use-package iedit
+  :ensure t)
+
+(use-package smartparens-config
+  :ensure smartparens
+  :config
+  (show-smartparens-global-mode t)
+  (smartparens-global-mode t))
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (global-auto-complete-mode t)
+    (ac-config-default)
+    (ac-flyspell-workaround)))
+
+(use-package flyspell
+  :ensure t
+  :config
+  (progn
+    (setq flyspell-issue-message-flag nil)
+    (setq ispell-extra-args '("--sug-mode=fast"))
+    (setq ispell-dictionary "british")
+    (add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC"))))
+
+(defun flyspell-check-next-highlighted-word ()
+  "Custom function to spell check next highlighted word"
+  (interactive)
+  (flyspell-goto-next-error)
+  (ispell-word))
+
+(use-package auto-yasnippet
+  :ensure t)
+
+(use-package magit
+  :ensure t)
+
+;; DWIM
+;; better commenting
+(defun better-comment-dwim ()
+  "Like 'comment-dwim', but toggle comment if cursor is not at end of line."
+  (interactive)
+  (if (region-active-p)
+      (comment-dwim nil)
+    (let (($lbp (line-beginning-position))
+          ($lep (line-end-position)))
+      (if (eq $lbp $lep)
+          (progn
+            (comment-dwim nil))
+        (if (eq (point) $lep)
+            (progn
+              (comment-dwim nil))
+          (progn
+            (comment-or-uncomment-region $lbp $lep)
+            (forward-line )))))))
+
+;; better narrowing and widening
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or
+defun, whichever applies first. Narrowing to
+org-src-block actually calls `org-edit-src-code'.
+With prefix P, don't widen, just narrow even if buffer
+is already narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning)
+                           (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if
+         ;; you don't want it.
+         (cond ((ignore-errors (org-edit-src-code) t)
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
+
+;; (define-key endless/toggle-map "n"
+  ;; #'narrow-or-widen-dwim)
+;; This line actually replaces Emacs' entire narrowing
+;; keymap, that's how much I like this command. Only
+;; copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
+;; (add-hook 'LaTeX-mode-hook
+;;           (lambda ()
+;;             (define-key LaTeX-mode-map "\C-xn"
+;;               nil)))
+
+(defun find-corresponding-file ()
+    "Find the file that corresponds to this one."
+    (interactive)
+    (setq CorrespondingFileName nil)
+    (setq BaseFileName (file-name-sans-extension buffer-file-name))
+    (if (string-match "\\.c" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".h")))
+    (if (string-match "\\.h" buffer-file-name)
+       (if (file-exists-p (concat BaseFileName ".c")) (setq CorrespondingFileName (concat BaseFileName ".c"))
+	   (setq CorrespondingFileName (concat BaseFileName ".cpp"))))
+    (if (string-match "\\.hin" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".cin")))
+    (if (string-match "\\.cin" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".hin")))
+    (if (string-match "\\.cpp" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".h")))
+    (if CorrespondingFileName (find-file CorrespondingFileName)
+       (error "Unable to find a corresponding file")))
+(defun find-corresponding-file-other-window ()
+  "Find the file that corresponds to this one."
+  (interactive)
+  (find-file-other-window buffer-file-name)
+  (find-corresponding-file)
+  (other-window -1))
+
+(defun server-shutdown ()
+  "Save buffers, quit, and shutdown server."
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs))
+
+;; GUI
+(setq inhibit-startup-message t)
+(setq frame-title-format "emacs")
+(setq initial-scratch-message "")
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; editing
+(electric-pair-mode 1)
+(delete-selection-mode 1)
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq comment-style 'extra-line)
+
+;; Keys
+(bind-keys*
+ ("<f5>" . revert-buffer)
+ ("C-c r" . query-replace)
+ ("M-g" . goto-line)
+ ("C-x C-c" . server-shutdown)
+ ("C-'" . better-comment-dwim)
+
+ ;; c/c++
+ ("C-c j" . compile)
+ ("C-c k" . next-error)
+ ("C-c s" . find-corresponding-file)
+ ("C-c S" . find-corresponding-file-other-window)
+
+ ;; flyspell
+ ("C-c o" . flyspell-mode)
+ ("C-c p" . flyspell-buffer)
+ ("C-c P" . ispell-word)
+ ("C-c C-M-p" . ispell)
+ ("C-c C-p" . flyspell-check-previous-highlighted-word)
+ ("C-c M-p" . flyspell-check-next-highlighted-word)
+
+ ;; auto-yasnippet
+ ("C-c w" . aya-create)
+ ("C-c e" . aya-expand)
+
+ ;; counsel
+ ("M-x" . counsel-M-x)
+ ("C-x C-f" . counsel-find-file)
+ ("C-x b" . list-buffers)
+ ("C-x C-b" . ivy-switch-buffer)
+
+ ;; misc
+ ("C-x g" . magit-status)
+ ("M-/" . undo-tree-visualize)
+ ("C-=" . er/expand-region)
+ ("C-x o" . switch-window)
+ )
+
+;; Saving
 ;; auto-save
 (defvar user-temporary-file-directory
   (concat temporary-file-directory "auto-save" "/"))
@@ -48,234 +295,3 @@
     (backup-buffer)))
 (add-hook 'before-save-hook  'force-backup-of-buffer)
 
-(setq smex-save-file "/tmp/.smex")
-
-(windmove-default-keybindings)
-(delete-selection-mode 1)
-(load-theme 'monokai t)
-
-(setq frame-title-format "emacs")
-(setq initial-scratch-message "")
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(show-paren-mode)
-(column-number-mode)
-(winner-mode t)
-(global-undo-tree-mode)
-(electric-pair-mode 1)
-
-(defun initialize-nlinum (&optional frame)
-  (require 'nlinum)
-  (add-hook 'prog-mode-hook 'nlinum-mode))
-(when (daemonp)
-  (add-hook 'window-setup-hook 'initialize-nlinum)
-  (defadvice make-frame (around toggle-nlinum-mode compile activate)
-    (nlinum-mode -1) ad-do-it (nlinum-mode 1)))
-
-;; maximise
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(powerline-center-theme)
-(setq powerline-default-separator 'wave)
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-(global-set-key (kbd "M-/") 'undo-tree-visualize)
-(global-set-key (kbd "C->") 'ace-jump-mode)
-
-(require 'multiple-cursors)
-(global-set-key (kbd "C-}") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-{") 'mc/mark-previous-like-this)
-
-(require 'switch-window)
-(global-set-key (kbd "C-c w") 'switch-window) 
-
-(require 'auto-complete)
-(defun auto-complete-mode-maybe ()
-  (unless (minibufferp (current-buffer))
-    (auto-complete-mode 1)))
-(global-auto-complete-mode t)
-(ac-flyspell-workaround)
-
-;; spell check
-(require 'flyspell)
-(setq flyspell-issue-message-flag nil)
-(setq ispell-extra-args '("--sug-mode=fast"))
-(setq ispell-dictionary "british")
-(add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC"))
-
-(global-set-key (kbd "C-x o") 'flyspell-mode)
-(global-set-key (kbd "C-x p") 'flyspell-buffer)
-(global-set-key (kbd "C-x P") 'ispell-word)
-(global-set-key (kbd "C-x C-M-p") 'ispell)
-(global-set-key (kbd "C-x C-p") 'flyspell-check-previous-highlighted-word)
-(defun flyspell-check-next-highlighted-word ()
-  "Custom function to spell check next highlighted word"
-  (interactive)
-  (flyspell-goto-next-error)
-  (ispell-word))
-(global-set-key (kbd "C-x M-p") 'flyspell-check-next-highlighted-word)
-
-;; enable move-text
-(move-text-default-bindings)
-
-(require 'ido)
-(ido-mode t)
-(setq ido-save-directory-list-file "/tmp/ido.last")
-
-(require 'org)
-(require 'ox)
-(require 'ox-latex)
-(setq org-indent-mode t)
-(setq org-hide-leading-stars t)
-(setq org-startup-indented t)
-
-(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "CANCELLED(c)" "|" "DONE(d)")))
-(setq org-todo-keyword-faces '(("CANCELLED" . "yellow")))
-
-(setq org-completion-use-ido t)
-(setq org-return-follows-link t)
-(setq org-log-done 'time)
-(setq org-log-into-drawer t)
-(setq org-image-actual-width nil)
-(setq org-startup-with-latex-preview t)
-
-;; indentation in src_blocks
-(setq org-src-tab-acts-natively t)
-
-(setq org-latex-create-formula-image-program 'dvipng)
-(org-babel-do-load-languages 'org-babel-load-languages '((latex . t)))
-
-;; Org following links
-(setq org-link-frame-setup
-   (quote
-    ((vm . vm-visit-folder-other-frame)
-     (vm-imap . vm-visit-imap-folder-other-frame)
-     (gnus . org-gnus-no-new-news)
-     (file . find-file)
-     (wl . wl-other-frame))))
-
-;; Make windmove work in org-mode
-(add-hook 'org-shiftup-final-hook 'windmove-up)
-(add-hook 'org-shiftleft-final-hook 'windmove-left)
-(add-hook 'org-shiftdown-final-hook 'windmove-down)
-(add-hook 'org-shiftright-final-hook 'windmove-right)
-
-(setq org-outline-path-complete-in-steps nil) 
-(setq org-refile-use-outline-path t)                 
-
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-(require 'wrap-region)
-(wrap-region-add-wrappers
-   '(("*" "*" nil (org-mode))
-     ("~" "~" nil (org-mode))
-     ("/" "/" nil (org-mode))
-     ("=" "=" nil (org-mode))
-     ("+" "+" nil (org-mode))
-     ("_" "_" nil (org-mode))
-     ("$" "$" nil (org-mode latex-mode))))
-(add-hook 'org-mode-hook 'wrap-region-mode)
-(add-hook 'latex-mode-hook 'wrap-region-mode)
-
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(require 'org-autolist)
-(add-hook 'org-mode-hook (lambda () (org-autolist-mode)))
-
-;; word-wrapping in org-mode
-(add-hook 'org-mode-hook #'(lambda () (visual-line-mode)))
-;; (defun t-word-wrap()
-;;   (turn-on-auto-fill)
-;;   (setq-default fill-column -1)
-;;   (setq auto-hscroll-mode nil))
-;; (add-hook 'text-mode-hook 't-word-wrap)
-
-;; stop emacs from creating session files
-(defadvice emacs-session-filename (around session-to-var activate)
-  (expand-file-name (concat "/tmp/session." session-id)))
-
-;; better commenting
-(defun better-comment-dwim ()
-  "Like `comment-dwim', but toggle comment if cursor is not at end of line."
-  (interactive)
-  (if (region-active-p)
-      (comment-dwim nil)
-    (let (($lbp (line-beginning-position))
-          ($lep (line-end-position)))
-      (if (eq $lbp $lep)
-          (progn
-            (comment-dwim nil))
-        (if (eq (point) $lep)
-            (progn
-              (comment-dwim nil))
-          (progn
-            (comment-or-uncomment-region $lbp $lep)
-            (forward-line )))))))
-(global-set-key (kbd "C-'") 'better-comment-dwim)
-
-;; set linux kernel style c comments
-(setq comment-style 'extra-line)
-
-(defun find-corresponding-file ()
-    "Find the file that corresponds to this one."
-    (interactive)
-    (setq CorrespondingFileName nil)
-    (setq BaseFileName (file-name-sans-extension buffer-file-name))
-    (if (string-match "\\.c" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".h")))
-    (if (string-match "\\.h" buffer-file-name)
-       (if (file-exists-p (concat BaseFileName ".c")) (setq CorrespondingFileName (concat BaseFileName ".c"))
-	   (setq CorrespondingFileName (concat BaseFileName ".cpp"))))
-    (if (string-match "\\.hin" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".cin")))
-    (if (string-match "\\.cin" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".hin")))
-    (if (string-match "\\.cpp" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".h")))
-    (if CorrespondingFileName (find-file CorrespondingFileName)
-       (error "Unable to find a corresponding file")))
-(defun find-corresponding-file-other-window ()
-  "Find the file that corresponds to this one."
-  (interactive)
-  (find-file-other-window buffer-file-name)
-  (find-corresponding-file)
-  (other-window -1))
-
-(global-set-key (kbd "C-c s") 'find-corresponding-file)
-(global-set-key (kbd "C-c S") 'find-corresponding-file-other-window)
-
-;; useful keybindings
-(global-set-key (kbd "C-c C-x C-c") 'server-shutdown)
-(global-set-key (kbd "C-c d") 'revert-buffer)
-(global-set-key (kbd "C-c r") 'query-replace)
-(global-set-key (kbd "M-g") 'goto-line)
-
-;; define function to shutdown emacs server instance
-(defun server-shutdown ()
-  "Save buffers, quit, and shutdown server."
-  (interactive)
-  (save-some-buffers)
-  (kill-emacs))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-buffer-menu t)
- '(inhibit-startup-screen 1)
- '(org-hide-emphasis-markers t)
- '(org-src-preserve-indentation t)
- '(org-startup-with-latex-preview nil)
- '(org-tags-column 0))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
