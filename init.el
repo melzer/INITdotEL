9f8327f65ce07f589d4c583d04e42cb51d62d8c3
Toby Law <contact.tobylaw@gmail.com>
2018-09-12 16:30:07 +1000

    evil-args

--- init.el
+++ init.el
@@ -174,6 +174,23 @@
   :after evil
   :ensure t)
 
+(use-package evil-args
+  :ensure t
+  :init
+  (progn
+    ;; bind evil-args text objects
+    (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
+    (define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
+
+    ;; bind evil-forward/backward-args
+    (define-key evil-normal-state-map "L" 'evil-forward-arg)
+    (define-key evil-normal-state-map "H" 'evil-backward-arg)
+    (define-key evil-motion-state-map "L" 'evil-forward-arg)
+    (define-key evil-motion-state-map "H" 'evil-backward-arg)
+
+    ;; bind evil-jump-out-args
+    (define-key evil-normal-state-map "K" 'evil-jump-out-args)))
+
 (use-package winner
   :ensure t
   :init (winner-mode))
