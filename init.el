42a23987aa38b513d0338baff3c7d9bcac7c5f44
Toby Law <contact.tobylaw@gmail.com>
2019-08-12 11:50:11 +1000

    added projectile

--- init.el
+++ init.el
@@ -60,6 +60,16 @@
   (use-package ivy-hydra
     :ensure t))
 
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (setq projectile-completion-system 'ivy)
+  (projectile-mode +1))
+
+(use-package ag
+  :ensure t)
+
 (use-package org
   :ensure t
   :config
